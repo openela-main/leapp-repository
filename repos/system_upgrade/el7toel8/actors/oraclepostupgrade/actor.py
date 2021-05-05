24bde167fc7b2ca65a54a888e0a13436f264c5b3
Mark Will <mark.will@oracle.com>
2021-05-05 09:00:00 -0700

    Add Upgrade Repositories
    This patch installs the repo file containing the
    OL8 upgrade repositories.
    
    Additional actor to rename the file post upgrade
    as it may interfere with repo file installed by
    oraclelinux-release-el8
    
    [Orabug: 32214409]
    
    Signed-off-by: Mark Will <mark.will@oracle.com>
    ------

---
+++ repos/system_upgrade/el7toel8/actors/oraclepostupgrade/actor.py
@@ -0,0 +1,39 @@
+import os
+import errno
+
+from leapp.actors import Actor
+from leapp.libraries.stdlib import run
+from leapp.reporting import Report, create_report
+from leapp import reporting
+from leapp.tags import FinalizationPhaseTag, IPUWorkflowTag
+
+
+class OraclePostUpgrade(Actor):
+    """
+    Rename the leapp upgrade repo file /etc/yum.repos.d/leapp-upgrade-repos-ol8.repo
+
+    Required as it may interfere with installed files from oraclelinux-release-el8.
+    """
+
+    name = 'oraclepostupgrade'
+    consumes = ()
+    produces = (Report,)
+    tags = (FinalizationPhaseTag, IPUWorkflowTag)
+
+    def process(self):
+        leapp_upgrade_repo_file = '/etc/yum.repos.d/leapp-upgrade-repos-ol8.repo'
+        leapp_save_file = '/etc/yum.repos.d/leapp-upgrade-repos-ol8.repo.save'
+        if os.path.isfile(leapp_upgrade_repo_file):
+            try:
+                os.rename(leapp_upgrade_repo_file,leapp_save_file)
+            except OSError as e:
+                if e.errno != errno.ENOENT:
+                    raise
+
+        create_report([
+            reporting.Title('Leapp upgrade file {} renamed'.format(leapp_upgrade_repo_file)),
+            reporting.Summary(
+                'The upgrade repo file has been renamed "{}"'.format(leapp_save_file)),
+            reporting.Tags([reporting.Tags.UPGRADE_PROCESS]),
+        ])
+
