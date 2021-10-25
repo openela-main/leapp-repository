1cbd4b225709fa62a2f102f03bb2a279b6659814
Mark Will <mark.will@oracle.com>
2021-10-25 09:00:00 -0700

    Oracle KVM Check
    This patch creates an actor that performs checks for installed
    Oracle KVM packages and ensures the correct KVM upgrade
    repository is enabled on the command line.
    
    Update for OL8 to OL9 upgrade.
    
    OraBug: 33599932
    
    Signed-off-by: Mark Will <mark.will@oracle.com>
    ------

---
+++ repos/system_upgrade/el8toel9/actors/checkkvm/actor.py
@@ -0,0 +1,74 @@
+from leapp.actors import Actor
+from leapp.dialogs import Dialog
+from leapp.models import DistributionSignedRPM, InstalledRPM
+from leapp.libraries import stdlib
+from leapp.libraries.stdlib import api
+from leapp.libraries.common.config import architecture
+from leapp.dialogs.components import BooleanComponent
+from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
+from leapp import reporting
+from leapp.reporting import Report, create_report
+import os
+import sys
+
+
+class CheckKVM(Actor):
+    """
+    Check if Oracle KVM is installed and ensure ol9_kvm_utils is enabled
+
+    """
+
+    name = 'check_kvm'
+    consumes = (DistributionSignedRPM,)
+    produces = (Report,)
+    tags = (ChecksPhaseTag, IPUWorkflowTag)
+
+    def process(self):
+        pkgs = self.get_oracle_kvm_pkgs()
+        if not pkgs:
+            api.current_logger().info('No Oracle KVM packages found')
+            return
+
+        oracle_kvm = self.is_kvm_utils_enabled()
+        api.current_logger().info('Oracle KVM repo result: {}'.format(oracle_kvm))
+        if oracle_kvm is False:
+            api.current_logger().warning('Oracle ol9_kvm_utils is not enabled')
+            self.produce_inhibitor(pkgs)
+
+    def get_oracle_kvm_pkgs(self):
+        """
+        Find Oracle KVM packages
+        """
+        rpms = next(api.consume(DistributionSignedRPM), DistributionSignedRPM()).items
+        kvm_packages = []
+
+        for pkg in rpms:
+            if pkg.repository == 'ol8_kvm_appstream':
+                api.current_logger().info('Oracle KVM package found {}'.format(pkg.name))
+                kvm_packages.append(pkg.name)
+
+        if kvm_packages:
+            return kvm_packages
+
+    def is_kvm_utils_enabled(self):
+        leapp_enabled_repos = os.getenv('LEAPP_ENABLE_REPOS','')
+        if (leapp_enabled_repos.find('ol9_kvm_utils') != -1):
+            api.current_logger().info('LEAPP_ENABLE_REPOS has ol9_kvm_utils is enabled')
+            return True
+        else:
+            return False
+
+    def produce_inhibitor(self,pkgs):
+        summary = 'Upgrade process was inhibited as the following Oracle KVM packages have been detected, '
+        summary += 'but Oracle KVM upgrade repository ol9_kvm_utils has not been enabled.\n{}'.format('\n'.join(pkgs))
+        create_report([
+            reporting.Title('Oracle KVM packages detected, upgrade inhibited.'),
+            reporting.Summary(summary),
+            reporting.Severity(reporting.Severity.HIGH),
+            reporting.Tags([
+                    reporting.Tags.SECURITY,
+                    reporting.Tags.TOOLS
+            ]),
+            reporting.Flags([reporting.Flags.INHIBITOR])
+        ])
+
