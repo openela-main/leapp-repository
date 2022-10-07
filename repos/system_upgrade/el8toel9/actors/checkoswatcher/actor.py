fed8a55e4c39e72bae7b3974b5d700f7fb7f5586
Mark Will <mark.will@oracle.com>
2022-10-07 09:00:00 -0700

    OSWatcher To PCP Actor
    OSWatcher package has been removed in OL9, this actor
    checks for the installation of oswatcher and requires
    confirmation of update to pcp from user.
    
    [Orabug: 34417308]
    
    Signed-off-by: Mark Will <mark.will@oracle.com>
    ------

---
+++ repos/system_upgrade/el8toel9/actors/checkoswatcher/actor.py
@@ -0,0 +1,105 @@
+from leapp.actors import Actor
+from leapp.dialogs import Dialog
+from leapp.models import DistributionSignedRPM, InstalledRPM
+from leapp.libraries import stdlib
+from leapp.libraries.stdlib import api
+from leapp.libraries.common.config import architecture
+from leapp.dialogs.components import BooleanComponent
+from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
+from leapp import reporting
+from leapp.reporting import Report, create_report
+import os
+import sys
+
+
+class CheckOSWatcher(Actor):
+    """
+    Check if oswatcher package is installed and notify of upgrade to pcp in OL9
+
+    """
+
+    name = 'check_oswatcher'
+    consumes = (DistributionSignedRPM,)
+    produces = (Report,)
+    tags = (ChecksPhaseTag, IPUWorkflowTag)
+    dialogs = (
+        Dialog(
+            scope='update_oswatcher_to_pcp',
+            reason='Confirmation',
+            components=(
+                BooleanComponent(
+                    key='confirm',
+                    label='Confirm upgrade of oswatcher package to pcp? '
+                          'If no, the upgrade process will be interrupted.',
+                    description='oswatcher package has been replaced by pcp in OL9'
+                                'Confirm upgrade of oswatcher package to pcp?',
+                    reason='oswatcher package has been replaced by pcp in OL9'
+                ),
+            )
+        ),
+    )
+
+    def process(self):
+        pkgs = self.get_oswatcher_packages()
+        if not pkgs:
+            return
+
+        confirm_upgrade = self.confirm()
+        if confirm_upgrade is True:
+            self.produce_report()
+        else:
+            self.produce_inhibitor()
+
+    def confirm(self):
+        answer = self.get_answers(self.dialogs[0])
+        return answer.get('confirm')
+
+    def get_oswatcher_packages(self):
+        """
+        Find oswatcher package
+        """
+        rpms = next(api.consume(DistributionSignedRPM), DistributionSignedRPM()).items
+        oswatcher_packages = []
+
+        for pkg in rpms:
+            if pkg.name == 'oswatcher':
+                api.current_logger().info('Oracle oswatcher package found {}'.format(pkg.name))
+                oswatcher_packages.append(pkg.name)
+
+        if oswatcher_packages:
+            return oswatcher_packages
+
+    def produce_report(self):
+        summary = 'OSWatcher package has been removed in OL9 and will be replaced by pcp.'
+        create_report([
+            reporting.Title('OSWatcher is removed from OL9.'),
+            reporting.Summary(summary),
+            reporting.ExternalLink(
+                title='Working With Performance Co-Pilot',
+                url='https://docs.oracle.com/en/operating-systems/oracle-linux/9/monitoring/monitoring-WorkingWithPerformanceCoPilot.html#pcp%22'
+            ),
+            reporting.Tags([
+                    reporting.Tags.SECURITY,
+                    reporting.Tags.TOOLS
+            ]),
+            reporting.Severity(reporting.Severity.HIGH)
+        ])
+
+    def produce_inhibitor(self):
+        summary = 'Upgrade process was inhibited as OSWatcher is no longer available in OL9. '
+        summary += 'Please confirm the update of the oswatcher package to pcp.'
+        create_report([
+            reporting.Title('OSWatcher is removed from OL9.'),
+            reporting.Summary(summary),
+            reporting.ExternalLink(
+                title='Working With Performance Co-Pilot',
+                url='https://docs.oracle.com/en/operating-systems/oracle-linux/9/monitoring/monitoring-WorkingWithPerformanceCoPilot.html#pcp%22'
+            ),
+            reporting.Tags([
+                    reporting.Tags.SECURITY,
+                    reporting.Tags.TOOLS
+            ]),
+            reporting.Severity(reporting.Severity.HIGH),
+            reporting.Flags([reporting.Flags.INHIBITOR])
+        ])
+
