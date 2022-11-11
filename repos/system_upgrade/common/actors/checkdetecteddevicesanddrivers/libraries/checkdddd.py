74952b4af33e565df6d01965ba373b9f396f870a
Mark Will <mark.will@oracle.com>
2022-11-11 09:00:00 -0700

    Replace Upstream Refs
    Replace upstream references.
    
    [Orabug: 34790352]
    
    Signed-off-by: Mark Will <mark.will@oracle.com>
    ------

--- repos/system_upgrade/common/actors/checkdetecteddevicesanddrivers/libraries/checkdddd.py
+++ repos/system_upgrade/common/actors/checkdetecteddevicesanddrivers/libraries/checkdddd.py
@@ -23,11 +23,11 @@ def create_inhibitors(inhibiting_entries):
         reporting.create_report([
             reporting.Title(
                 'Leapp detected loaded kernel drivers which have been removed '
-                'in RHEL {}. Upgrade cannot proceed.'.format(get_target_major_version())
+                'in OL {}. Upgrade cannot proceed.'.format(get_target_major_version())
             ),
             reporting.Summary(
                 (
-                    'Support for the following RHEL {source} device drivers has been removed in RHEL {target}:\n'
+                    'Support for the following OL {source} device drivers has been removed in OL {target}:\n'
                     '     - {drivers}\n'
                 ).format(
                     drivers='\n     - '.join([entry.driver_name for entry in drivers]),
@@ -45,12 +45,12 @@ def create_inhibitors(inhibiting_entries):
     if devices:
         reporting.create_report([
             reporting.Title(
-                'Leapp detected devices which are no longer supported in RHEL {}. Upgrade cannot proceed.'.format(
+                'Leapp detected devices which are no longer supported in OL {}. Upgrade cannot proceed.'.format(
                     get_target_major_version())
             ),
             reporting.Summary(
                 (
-                    'Support for the following devices has been removed in RHEL {target}:\n'
+                    'Support for the following devices has been removed in OL {target}:\n'
                     '     - {devices}\n'
                 ).format(
                     devices='\n     - '.join(['{name} ({pci})'.format(name=entry.device_name,
@@ -68,12 +68,12 @@ def create_inhibitors(inhibiting_entries):
     if cpus:
         reporting.create_report([
             reporting.Title(
-                'Leapp detected a processor which is no longer supported in RHEL {}. Upgrade cannot proceed.'.format(
+                'Leapp detected a processor which is no longer supported in OL {}. Upgrade cannot proceed.'.format(
                     get_target_major_version())
             ),
             reporting.Summary(
                 (
-                    'Support for the following processors has been removed in RHEL {target}:\n'
+                    'Support for the following processors has been removed in OL {target}:\n'
                     '     - {processors}\n'
                 ).format(
                     processors='\n     - '.join([entry.device_name for entry in cpus]),
@@ -95,12 +95,12 @@ def create_warnings(unmaintained_entries):
     if drivers:
         reporting.create_report([
             reporting.Title(
-                'Leapp detected loaded kernel drivers which are no longer maintained in RHEL {}.'.format(
+                'Leapp detected loaded kernel drivers which are no longer maintained in OL {}.'.format(
                     get_target_major_version())
             ),
             reporting.Summary(
                 (
-                    'The following RHEL {source} device drivers are no longer maintained RHEL {target}:\n'
+                    'The following OL {source} device drivers are no longer maintained OL {target}:\n'
                     '     - {drivers}\n'
                 ).format(
                     drivers='\n     - '.join([entry.driver_name for entry in drivers]),
@@ -117,12 +117,12 @@ def create_warnings(unmaintained_entries):
     if devices:
         reporting.create_report([
             reporting.Title(
-                'Leapp detected devices which are no longer maintained in RHEL {}'.format(
+                'Leapp detected devices which are no longer maintained in OL {}'.format(
                     get_target_major_version())
             ),
             reporting.Summary(
                 (
-                    'The support for the following devices has been removed in RHEL {target} and '
+                    'The support for the following devices has been removed in OL {target} and '
                     'are no longer maintained:\n     - {devices}\n'
                 ).format(
                     devices='\n     - '.join(['{name} ({pci})'.format(name=entry.device_name,
@@ -139,12 +139,12 @@ def create_warnings(unmaintained_entries):
     if cpus:
         reporting.create_report([
             reporting.Title(
-                'Leapp detected a processor which is no longer maintained in RHEL {}.'.format(
+                'Leapp detected a processor which is no longer maintained in OL {}.'.format(
                     get_target_major_version())
             ),
             reporting.Summary(
                 (
-                    'The following processors are no longer maintained in RHEL {target}:\n'
+                    'The following processors are no longer maintained in OL {target}:\n'
                     '     - {processors}\n'
                 ).format(
                     processors='\n     - '.join([entry.device_name for entry in cpus]),
