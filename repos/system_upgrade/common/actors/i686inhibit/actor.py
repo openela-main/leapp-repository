d43f13cdd710b61ea28d0badf535560795c51f46
Joseph Oliver <joseph.oliver@oracle.com>
2023-10-26 14:37:00 +0000

    Implement I686 error inhibitor
    Implemented an actor to inhibit upgrade of OL8 if problematic I686 packages are installed on the system to prevent dependency issues on upgrade.
    
    Orabug: 35741807
    Orabug: 35741727
    Orabug: 35724480
    
    Reviewed-by: Laurence Rochfort <laurence.rochfort@oracle.com>
    Signed-off-by: Joseph Oliver <joseph.oliver@oracle.com>

---
+++ repos/system_upgrade/common/actors/i686inhibit/actor.py
@@ -0,0 +1,52 @@
+from leapp import reporting
+from leapp.actors import Actor
+from leapp.libraries.common import rpms
+from leapp.models import InstalledRPM
+from leapp.tags import FactsPhaseTag, IPUWorkflowTag
+from leapp.reporting import Report
+
+class i686Inhibit(Actor):
+    """
+    Checks for problematic i686 rpms that will cause a dependency related failure on upgrade.
+    """
+
+    name = 'i686inhibit'
+    consumes = ()
+    produces = (Report,)
+    tags = (IPUWorkflowTag, FactsPhaseTag)
+
+    def process(self):
+        output = rpms.get_installed_rpms()
+
+        result = InstalledRPM()
+        issuePackages = ["ibacm","usbguard","gobject-introspection-devel"]
+        issueList = []
+        for entry in output:
+            entry = entry.strip()
+            if not entry:
+                continue
+            name, version, release, epoch, packager, arch, pgpsig = entry.split('|')
+            for item in issuePackages:
+                if item in name:
+                    if arch == "i686":
+                       issueList.append(name + '.' + arch)
+        if issueList:
+            issueList = str(issueList).strip("[]")
+            issueList = issueList.replace("'","")
+            warnMsg = 'rpm(s) ' + issueList + ' for i686 architecture installed. This will cause dependency failure on upgrade.'
+            self.log.warning(warnMsg)
+            reporting.create_report(
+                [
+                    reporting.Title("Problematic i686 rpms installed"),
+                    reporting.Summary(warnMsg),
+                    reporting.Severity(reporting.Severity.HIGH),
+                    reporting.Groups([reporting.Groups.REPOSITORY]),
+                    reporting.Groups([reporting.Groups.INHIBITOR]),
+                    reporting.Remediation(
+                        hint=(
+                            "Please run command #dnf remove for all conflicting packages before upgrade, e.g. #dnf remove " + issueList.replace(",","")
+                        )
+                    ),
+                ]
+            )
+
