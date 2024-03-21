eb336964840ae0514360378e13f7048d69a2404c
Alexander Burmashev <alexander.burmashev@oracle.com>
2024-03-21 03:28:15 -0700

    Restore OSMS backed up repo if needed
    Orabug: 36428674
    Signed-off-by: Alex Burmashev <alexander.burmashev@oracle.com>

--- repos/system_upgrade/el7toel8/actors/osmsprep/actor.py
+++ repos/system_upgrade/el7toel8/actors/osmsprep/actor.py
@@ -4,6 +4,7 @@ from leapp.libraries.stdlib import run, api
 from leapp.tags import ChecksPhaseTag, IPUWorkflowTag
 from leapp import reporting
 from leapp.reporting import Report, create_report
+import os.path, shutil
 
 class OSMSprepRepos(Actor):
     """
@@ -25,9 +26,14 @@ class OSMSprepRepos(Actor):
     def add_leapp_repo(self):
         configfile = '/etc/oracle-cloud-agent/plugins/osms/ignored_repos.conf'
         leapp_repo_name = 'leapp-upgrade-repos-ol8.repo\n'
-        api.current_logger().warning("Upgrade of OCI manage instance is requested.\nAdding leapp-upgrade-repos-ol8.repo to\n/etc/oracle-cloud-agent/plugins/osms/ignored_repos.conf\nand restarting oracle-cloud-agent")
+        leapp_repo_file = '/etc/yum.repos.d/leapp-upgrade-repos-ol8.repo'
+        leapp_repo_file_osms_backup = '/etc/yum.repos.d/leapp-upgrade-repos-ol8.repo.osms-backup'
+        api.current_logger().warning("Upgrade of OCI managed instance is requested. Adding leapp-upgrade-repos-ol8.repo to /etc/oracle-cloud-agent/plugins/osms/ignored_repos.conf and restarting oracle-cloud-agent")
         fconfig = open(configfile, "a")
         if leapp_repo_name not in open(configfile).read():
             fconfig.write(leapp_repo_name)
         run(['systemctl', 'restart', 'oracle-cloud-agent'])
+        if not os.path.isfile(leapp_repo_file) and os.path.isfile(leapp_repo_file_osms_backup):
+            api.current_logger().warning("leapp repository file %s not found, but it's backup %s found, restoring leapp repository file to proceed with upgrade", leapp_repo_file, leapp_repo_file_osms_backup)
+            shutil.copyfile(leapp_repo_file_osms_backup, leapp_repo_file)
         return
