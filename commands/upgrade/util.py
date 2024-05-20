6a86671e5787567b5baafd845872f381f22d80c2
Alex Burmashev <alexander.burmashev@oracle.com>
2024-05-20 09:48:33 +0000

    Add Oracle Repos parameters
    This patch add arguments to the leapp preupgrade and upgrade command to enable standard repos.
    
    Updated for ol8 to ol9 upgrade
    
    Signed-off-by: Mark Will <mark.will@oracle.com>
    Modified-by: Joseph Oliver <joseph.oliver@oracle.com>

--- commands/upgrade/util.py
+++ commands/upgrade/util.py
@@ -5,6 +5,7 @@ import os
 import shutil
 import tarfile
 from datetime import datetime
+import platform
 
 from leapp.cli.commands import command_utils
 from leapp.cli.commands.config import get_config
@@ -195,6 +196,26 @@ def prepare_configuration(args):
 
     if args.enablerepo:
         os.environ['LEAPP_ENABLE_REPOS'] = ','.join(args.enablerepo)
+    if args.oci and args.oraclelinux:
+        raise CommandError('--oci and --oraclelinux cannot be run together')
+    if args.oci:
+        if platform.machine() == 'aarch64':
+            OCI_REPOS = os.getenv('LEAPP_OCI_REPOS', 'ol9_baseos_latest,ol9_appstream,ol9_addons,ol9_ksplice,ol9_oci_included')
+        else:
+            OCI_REPOS = os.getenv('LEAPP_OCI_REPOS', 'ol9_baseos_latest,ol9_appstream,ol9_UEKR7,ol9_addons,ol9_ksplice,ol9_oci_included')
+        if args.enablerepo:
+            os.environ['LEAPP_ENABLE_REPOS'] = OCI_REPOS + "," + (','.join(args.enablerepo))
+        else:
+            os.environ['LEAPP_ENABLE_REPOS'] = OCI_REPOS
+    if args.oraclelinux:
+        if platform.machine() == 'aarch64':
+            OL_REPOS = os.getenv('LEAPP_ORACLELINUX_REPOS', 'ol9_baseos_latest,ol9_appstream')
+        else:
+            OL_REPOS = os.getenv('LEAPP_ORACLELINUX_REPOS', 'ol9_baseos_latest,ol9_appstream,ol9_UEKR7')
+        if args.enablerepo:
+            os.environ['LEAPP_ENABLE_REPOS'] = OL_REPOS + "," + (','.join(args.enablerepo))
+        else:
+            os.environ['LEAPP_ENABLE_REPOS'] = OL_REPOS
 
     if 'LEAPP_TARGET_PRODUCT_CHANNEL' not in os.environ:
         os.environ['LEAPP_TARGET_PRODUCT_CHANNEL'] = 'ga'
